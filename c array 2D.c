5a420bf0c7315576d2e187053f9ef12d6f14a82a
James Gicho Mwangi <gichojimmy05@gmail.com>
2024-10-18 13:20:36 +0300

    Add files via upload

---
+++ c array 2D.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int i,j;
+	int marks[2][3]={{5,15,20},{30,50,70}};
+	
+
+	for(i=0;i<2;i++){
+		for(j=0;j<3;j++){
+		printf("marks[%d][%d]=%d\n",i,j,markss[i][j]);
+		}
+	}
+
+	return 0;
+}
